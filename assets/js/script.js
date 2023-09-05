0e392488a51b8e2d1178ab43d37c44598aba625e
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-05 18:02:42 -0500

    changed api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,16 @@ var todayWeatherTemp = $("#today-weather-temp");
 var todayWeatherWind = $("#today-weather-wind");
 var todayWeatherHumid = $("#today-weather-humidity");
 
-var apiKey = $("27918b0a1eff4c123d61cdad98ffa668");
-var apiURL = 
+var apiURLCurrent = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=`
+
+var apiURLForecast = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=`
 
 function getCurrentWeather (event) {
 
     event.preventDefault();
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?appid=$apiKey&units=imperial&q=`;"
 
 $.ajax({
-url:requestURL + userInput.val(),
+url:apiURLCurrent + userInput.val(),
 method: "GET"
 })
 .then(function(data) {
