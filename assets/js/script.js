e508685ec65feee4bf6cd4b2787bb6dfc8ee9538
JMADA257 <jadams1812@hotmail.com>
2023-09-06 10:13:47 -0600

    Updated js for daily weather to show the icon image

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,11 @@ var citySearchBtn = $("#search-button");
 var cityDropdownEl = $("#city-dropdown");
 
 var todayWeatherCityName = $("#today-weather-city-name");
-var todayWeatherIcon = $("today-weather-icon");
+var todayWeatherIcon = $("#today-weather-icon");
 var todayWeatherTemp = $("#today-weather-temp");
 var todayWeatherWind = $("#today-weather-wind");
 var todayWeatherHumid = $("#today-weather-humidity");
 var forecastContainer = $("#forecast-contain");
-
 
 var apiKey = "fd14e40bfd6c1460a77a88f01383cf1e";
 var dailyURL =
@@ -41,6 +40,7 @@ function currentWeather(city) {
     var temp = data.main.temp;
     var wind = data.wind.speed;
     var humidity = data.main.humidity;
+    var cWIcon = $("#cWeatherIcon");
 
     //appending the current city here for daily
 
@@ -48,6 +48,11 @@ function currentWeather(city) {
     todayWeatherTemp.text(temp);
     todayWeatherWind.text(wind);
     todayWeatherHumid.text(humidity);
+    todayWeatherIcon.attr(
+      "src",
+      "https://openweathermap.org/img/wn/" + icon + ".png"
+    );
+    
   });
 }
 
@@ -59,28 +64,30 @@ function upcomingWeather(city) {
     method: "GET",
   }).then(function (data) {
     console.log(data);
-    data.list.forEach ((value) => {
+    data.list.forEach((value) => {
       if (!value.dt_txt.includes("15:00:00")) {
         return;
       }
 
       var card = $('<div class="card">');
       var forecastIcon = $('<img class ="card-img-top" alt="weather icon">');
-      var cardBody = $('<div class="card-body">')
-      var date = $('<h5 class="card-title">')
+      var cardBody = $('<div class="card-body">');
+      var date = $('<h5 class="card-title">');
       var forecastTemp = $('<p class="card-text">');
       var forecastWind = $('<p class="card-text">');
       var forecastHumid = $('<p class="card-text">');
       var iconName = value.weather[0].icon;
-      
-      forecastContainer.append(card);
-      forecastIcon.attr("src","https://openweathermap.org/img/wn/" + iconName + ".png")
-      card.append(forecastIcon);
 
-    })
+      forecastContainer.append(card);
+      forecastIcon.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + iconName + ".png"
+      );
+      card.append(forecastIcon);
+    });
   });
 }
-upcomingWeather("Duluth");
+upcomingWeather("");
 // Card Template
 
 //use the .empty method to clear out the cards
